ec7d2a988542243fa95cae1ee7ff8712beddfc3d
Cropsi <cropsi@icloud.com>
2026-09-15 16:35:58 +0300

    Добавлен компонент карты + базоваые элементы навигации
    Не забудьте спосить env -NEXT_PUBLIC_MAPTILER_API_KEY

---
+++ frontend/src/components/MapComponent.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+  FullscreenControl,
+  GeolocateControl,
+  Map,
+  NavigationControl,
+  ScaleControl,
+} from "@vis.gl/react-maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { ProjectionControl } from "./controls/ProjectionControl";
+
+export default function MapComponent({ mapRef }) {
+  return (
+    <Map
+      styleDiffing
+      hash
+      keyboard
+      light={{
+        anchor: "viewport",
+        "position-transition": { duration: 1000 },
+        color: "#FFD38A",
+      }}
+      attributionControl={false}
+      sky={{
+        "sky-color": "#88C6FC",
+      }}
+      id="mainMap"
+      maxPitch={75}
+      reuseMaps
+      zoomSnap={1}
+      maxZoom={20}
+      minZoom={0}
+      ref={mapRef}
+      // Надо будет вынести для разных стилей карты в отдельный обьект
+      mapStyle={`https://api.maptiler.com/maps/01a0a53f-a24b-7778-b5e1-b59ba3d6f612/style.json?key=${process.env.NEXT_PUBLIC_MAPTILER_API_KEY}`}
+    >
+      <NavigationControl visualizePitch visualizeRoll></NavigationControl>
+      <FullscreenControl></FullscreenControl>
+      <GeolocateControl></GeolocateControl>
+      <ScaleControl></ScaleControl>
+      <ProjectionControl></ProjectionControl>
+    </Map>
+  );
+}
