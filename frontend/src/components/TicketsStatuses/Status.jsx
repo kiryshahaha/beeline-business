170c85f9d1ce031865b5c3304d09b00cdac0f21c
kiryshahaha <kiryagushchin@icloud.com>
2026-09-24 00:31:55 +0300

    feat(frontend): панель заявок (TicketsStatuses)

--- frontend/src/components/TicketsStatuses/Status.jsx
+++ frontend/src/components/TicketsStatuses/Status.jsx
@@ -1,8 +1,21 @@
 import styles from "./TicketsStatuses.module.css"
 
-const Status = ({ label, count, variant }) => {
+const Status = ({ label, count, variant, onClick, isInteractive, isInactive }) => {
     return (
-        <div className={`${styles.statusContainer} ${styles[variant]}`}>
+        <div 
+            className={`
+                ${styles.statusContainer} 
+                ${styles[variant]} 
+                ${isInteractive ? styles.interactive : ""} 
+                ${isInactive ? styles.inactive : ""}
+            `}
+            onClick={(e) => {
+                if (onClick && isInteractive) {
+                    e.stopPropagation(); // Чтобы клик по таблетке не закрывал/открывал общую плашку
+                    onClick();
+                }
+            }}
+        >
             <span className={styles.status}>{label}</span>
             <span className={styles.count}>({count})</span>
         </div>
