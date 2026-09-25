3168a2ca8c9f6d4454dd95467f0d7154094a08f7
m2dyt <roman16050811@mail.ru>
2026-09-25 16:40:59 +0300

    Fix ruff errors in migration

--- backend/migrations/versions/0d5647f7d664_merge_multiple_heads_from_t15_and_t07.py
+++ backend/migrations/versions/0d5647f7d664_merge_multiple_heads_from_t15_and_t07.py
@@ -6,10 +6,6 @@ Revises: 0023, 6a36204671b2
 
 from collections.abc import Sequence
 
-import sqlalchemy as sa
-from alembic import op
-
-
 revision: str = '0d5647f7d664'
 down_revision: str | None = ('0023', '6a36204671b2')
 branch_labels: str | Sequence[str] | None = None
