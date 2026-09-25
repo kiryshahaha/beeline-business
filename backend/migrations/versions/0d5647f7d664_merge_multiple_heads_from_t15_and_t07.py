a06127b9f30154188a771f86be84113ca5eaafac
m2dyt <roman16050811@mail.ru>
2026-09-25 16:48:10 +0300

    Format migration

--- backend/migrations/versions/0d5647f7d664_merge_multiple_heads_from_t15_and_t07.py
+++ backend/migrations/versions/0d5647f7d664_merge_multiple_heads_from_t15_and_t07.py
@@ -6,8 +6,8 @@ Revises: 0023, 6a36204671b2
 
 from collections.abc import Sequence
 
-revision: str = '0d5647f7d664'
-down_revision: str | None = ('0023', '6a36204671b2')
+revision: str = "0d5647f7d664"
+down_revision: str | None = ("0023", "6a36204671b2")
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
