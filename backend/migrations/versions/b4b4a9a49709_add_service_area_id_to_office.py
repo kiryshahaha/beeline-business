230f50810cf83587ccd876297f175ec1e0783efc
m2dyt <roman16050811@mail.ru>
2026-09-26 01:18:33 +0300

    Fix alembic multiple heads from merged branches

--- backend/migrations/versions/b4b4a9a49709_add_service_area_id_to_office.py
+++ backend/migrations/versions/b4b4a9a49709_add_service_area_id_to_office.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "b4b4a9a49709"
-down_revision: str | None = "0025"
+down_revision: str | None = "0026"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
