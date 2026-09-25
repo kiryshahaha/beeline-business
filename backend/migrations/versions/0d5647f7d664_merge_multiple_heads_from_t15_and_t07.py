7bda25a71524538b8ed2c1c83591a6068134cf39
m2dyt <roman16050811@mail.ru>
2026-09-25 16:38:43 +0300

    Merge alembic heads from T15 and T07

---
+++ backend/migrations/versions/0d5647f7d664_merge_multiple_heads_from_t15_and_t07.py
@@ -0,0 +1,24 @@
+"""Merge multiple heads from T15 and T07
+
+Revision ID: 0d5647f7d664
+Revises: 0023, 6a36204671b2
+"""
+
+from collections.abc import Sequence
+
+import sqlalchemy as sa
+from alembic import op
+
+
+revision: str = '0d5647f7d664'
+down_revision: str | None = ('0023', '6a36204671b2')
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
